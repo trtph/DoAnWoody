d71f94cf44b20d1288cc051582d7b3addf9d6074
HoangTruong250501 <hoangtn19411c@st.uel.edu.vn>
2021-12-04 22:08:07 +0700

    add shipping layout, payment layout

--- app/src/main/java/com/example/woodygroupapplication/Checkout_Layout.java
+++ app/src/main/java/com/example/woodygroupapplication/Checkout_Layout.java
@@ -1,15 +1,15 @@
 package com.example.woodygroupapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Checkout_Layout extends AppCompatActivity {
 
-    ImageView btnChangeAdd, btnBackCheckout;
+    ImageView btnChangeAdd, btnBackCheckout, btnChangePayment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,25 @@ public class Checkout_Layout extends AppCompatActivity {
         setContentView(R.layout.activity_checkout_layout);
 
         btnChangeAdd = findViewById(R.id.btnChangeAdd);
+        btnChangePayment = findViewById(R.id.btnChangePayment);
+        btnBackCheckout = findViewById(R.id.btnBackCheckout);
 
         btnChangeAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Checkout_Layout.this, AddShipping_Layout.class));
+            }
+        });
+        btnChangePayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Checkout_Layout.this, PaymentMethod_Layout.class));
+            }
+        });
+        btnBackCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Checkout_Layout.this, MainActivity.class));
             }
         });
 
