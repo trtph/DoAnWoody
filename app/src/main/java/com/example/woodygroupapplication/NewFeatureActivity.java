55c9e424a2fcc40f83edbf2b85287004e2fec4ad
MongNguyen19411C <mongnt19411c@st.uel.edu.vn>
2021-12-19 18:21:21 +0700

    edit noti

--- app/src/main/java/com/example/woodygroupapplication/NewFeatureActivity.java
+++ app/src/main/java/com/example/woodygroupapplication/NewFeatureActivity.java
@@ -8,18 +8,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class NewFeatureActivity extends AppCompatActivity {
 
-    Button btnBack;
+    LinearLayout lvBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_feature);
-        btnBack = findViewById(R.id.btnBack);
+        lvBack = findViewById(R.id.lvBack);
 
-        btnBack.setOnClickListener(new View.OnClickListener() {
+        lvBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
--- app/src/main/java/com/example/woodygroupapplication/NotiActivity.java
+++ app/src/main/java/com/example/woodygroupapplication/NotiActivity.java
@@ -9,26 +9,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class NotiActivity extends AppCompatActivity {
 
-    ImageView imvBackHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noti);
-        linkViews();
-        addEvents();
+
     }
 
-    private void linkViews() {
-        imvBackHome = findViewById(R.id.imvBackHome);
-    }
-
-    private void addEvents() {
-        imvBackHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(NotiActivity.this, MainActivity.class));
-            }
-        });
-    }
 }
