fc5ee73927cc1a3b6b8de2813b08b62a8cd8ac26
pnhun01 <nhungpphh@gmail.com>
2021-12-20 23:28:13 +0700

    add Utils

--- app/src/main/java/com/example/woodygroupapplication/Checkout_Layout.java
+++ app/src/main/java/com/example/woodygroupapplication/Checkout_Layout.java
@@ -4,21 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.utils.AppUtils;
+import com.example.utils.NameUtils;
 
 public class Checkout_Layout extends AppCompatActivity {
 
     ImageView btnChangeAdd, btnBackCheckout, btnChangePayment;
+    TextView textView7,txtAddress,txtNameAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_layout);
 
+        textView7=findViewById(R.id.textView7);
+        txtAddress=findViewById(R.id.txtAddress);
+        txtNameAdd=findViewById(R.id.txtNameAdd);
+
         btnChangeAdd = findViewById(R.id.btnChangeAdd);
         btnChangePayment = findViewById(R.id.btnChangePayment);
         btnBackCheckout = findViewById(R.id.btnBackCheckout);
+
+        if(getIntent().getExtras() !=null) {
+            AppUtils appUtils = (AppUtils) getIntent().getExtras().get("object_card");
+            textView7.setText(appUtils.getCard());
+        }
+
+        if(getIntent().getExtras() !=null) {
+            NameUtils nameUtils = (NameUtils) getIntent().getExtras().get("object_name");
+            txtNameAdd.setText(nameUtils.getName());
+        }
+
 
         btnChangeAdd.setOnClickListener(new View.OnClickListener() {
             @Override
